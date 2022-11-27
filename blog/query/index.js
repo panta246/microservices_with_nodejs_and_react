4af298a0f8e0aa30c1a5e4857552978f58c7b5c7
Stefan Pantić <stefan.pantic@live.com>
2022-11-27 22:16:55 +0100

    fix

--- blog/query/index.js
+++ blog/query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {}
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-})
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data
 
@@ -28,8 +23,8 @@ app.post('/events', (req, res) => {
         post.comments.push({ id, content, status })
     }
 
-    if(type === 'CommentUpdated'){
-        const {id, content, postId, status} = data;
+    if (type === 'CommentUpdated') {
+        const { id, content, postId, status } = data;
 
         const post = posts[postId];
         const comment = post.comments.find(comment => {
@@ -38,9 +33,30 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body
+    handleEvent(type, data)
     res.send({})
 })
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listening at port 4002')
+    try {
+        const res = await axios.get('http://localhost:4005/events');
+
+    for(let event of res.data){
+        console.log('Processing event:', event.type);
+
+        handleEvent(event.type, event.data)
+    }
+    } catch (error) {
+        console.log(error.message)
+    }
+    
 })
